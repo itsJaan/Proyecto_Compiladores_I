c99f23afd7f6dca97706497c63d252a35a52ab0a
willi <willi@LAPTOP-2TJ1FHS3>
2021-03-07 01:15:15 -0600

    arreglando el jflex para usar bien el j

---
+++ src/proyectocompiladores/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocompiladores;
+
+import java.io.File;
+
+/**
+ *
+ * @author willi
+ */
+public class Main {
+    public static void main(String[] args) {
+        String ruta = "C:/Users/willi/OneDrive/Documents/NetBeansProjects/Proyecto_Compiladores_I-master/src/proyectocompiladores/Analizador_Lexico.flex";
+        lexer(ruta);
+        
+        formPrincipal form = new formPrincipal();
+        form.setVisible(true);
+        form.show();
+    }
+    
+    public static void lexer(String ruta){
+        File archivo = new File(ruta);
+        JFlex.Main.generate(archivo);
+    }
+}
